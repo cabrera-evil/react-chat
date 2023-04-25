bd8adb27613558b6dc37c4fefedc5b58af4a0ccf
cabrera-evil <douglascabrera.dev@gmail.com>
2023-04-25 17:54:02 -0600

    Fix: app indent

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Login from './pages/Login'
 import Home from './pages/Home'
 // Import context
 import { AuthContext } from "./context/AuthContext";
-
 // Import styles
 import './assets/scss/App.scss'
 
