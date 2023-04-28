379605266b34aaa18377cc795962520b5768e4b7
cabrera-evil <douglascabrera.dev@gmail.com>
2023-04-28 17:47:11 -0600

    Update: register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,35 +1,61 @@
-// Import modules
 import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-// Import firebase
+import Add from "../img/addAvatar.png";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
+    const [err, setErr] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
+        setLoading(true);
         e.preventDefault();
         const displayName = e.target[0].value;
         const email = e.target[1].value;
         const password = e.target[2].value;
+        const file = e.target[3].files[0];
 
         try {
             //Create user
-            createUserWithEmailAndPassword(auth, email, password)
-                .then((userCredential) => {
-                    // Signed in 
-                    const user = userCredential.user;
+            const res = await createUserWithEmailAndPassword(auth, email, password);
 
-                    console.log(user);
-                    // ...
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    // ..
+            //Create a unique image name
+            const date = new Date().getTime();
+            const storageRef = ref(storage, `${displayName + date}`);
+
+            await uploadBytesResumable(storageRef, file).then(() => {
+                getDownloadURL(storageRef).then(async (downloadURL) => {
+                    try {
+                        //Update profile
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                        });
+                        //create user on firestore
+                        await setDoc(doc(db, "users", res.user.uid), {
+                            uid: res.user.uid,
+                            displayName,
+                            email,
+                            photoURL: downloadURL,
+                        });
+
+                        //create empty user chats on firestore
+                        await setDoc(doc(db, "userChats", res.user.uid), {});
+                        navigate("/");
+                    } catch (err) {
+                        console.log(err);
+                        setErr(true);
+                        setLoading(false);
+                    }
                 });
+            });
         } catch (err) {
-            console.error(err);
+            setErr(true);
+            setLoading(false);
         }
     };
 
@@ -39,13 +65,20 @@ const Register = () => {
                 <span className="logo">Evil Chat</span>
                 <span className="title">Register</span>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Username" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button className="btn">Sing up</button>
+                    <input required type="text" placeholder="display name" />
+                    <input required type="email" placeholder="email" />
+                    <input required type="password" placeholder="password" />
+                    <input required style={{ display: "none" }} type="file" id="file" />
+                    <label htmlFor="file">
+                        <img src={Add} alt="" />
+                        <span>Add an avatar</span>
+                    </label>
+                    <button disabled={loading}>Sign up</button>
+                    {loading && "Uploading and compressing the image please wait..."}
+                    {err && <span>Something went wrong</span>}
                 </form>
                 <p>
-                    You do have an account? <Link to="/login">Login</Link>
+                    You do have an account? <Link to="/register">Login</Link>
                 </p>
             </div>
         </div>
