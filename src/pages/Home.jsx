4f64ba16788018e3fd98f4dda7e30a1fa7c00e42
cabrera-evil <douglascabrera.dev@gmail.com>
2023-04-28 17:57:55 -0600

    Update: home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,16 @@
-// Import modules
 import React from 'react'
-// Import components
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
 
 const Home = () => {
     return (
         <div className='home'>
-            <div className='container'>
+            <div className="container">
                 <Sidebar />
-                <Chat />    
+                <Chat />
             </div>
         </div>
-    );
+    )
 }
 
-export default Home;
+export default Home
