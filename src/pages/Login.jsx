c0a8d322194b378b843f0e49eac22d0c7e10846f
cabrera-evil <douglascabrera.dev@gmail.com>
2023-04-28 17:47:17 -0600

    Update: login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,46 +1,36 @@
-// Import modules
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-// Import firebase
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
 const Login = () => {
+    const [err, setErr] = useState(false);
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         const email = e.target[0].value;
         const password = e.target[1].value;
 
         try {
-            signInWithEmailAndPassword(auth, email, password)
-                .then((userCredential) => {
-                    // Signed in 
-                    const user = userCredential.user;
-                    console.log(user);
-                    // ...
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                });
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/")
         } catch (err) {
-            console.error(err);
+            setErr(true);
         }
     };
-
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">Evil Chat</span>
                 <span className="title">Login</span>
                 <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button className="btn">Sing in</button>
+                    <input type="email" placeholder="email" />
+                    <input type="password" placeholder="password" />
+                    <button>Sign in</button>
+                    {err && <span>Something went wrong</span>}
                 </form>
-                <p>
-                    You don't have an account? <Link to="/register">Register</Link>
-                </p>
+                <p>You don't have an account? <Link to="/register">Register</Link></p>
             </div>
         </div>
     );
